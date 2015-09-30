794f089045b185653f26d8d34730a44f6cc2222b
Piyush Aggarwal <piyush8311@gmail.com>
2015-09-30 17:53:24 +0530

    Completed send_packet and added close_socket

--- icmp.c
+++ icmp.c
@@ -2,11 +2,14 @@
  *  icmp.c
  */
 
+#include "icmp.h"
 #include <stdint.h>
 #include <string.h>
 #include <netinet/ip.h>
 #include <netinet/ip_icmp.h>
 #include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
 
 // The socket fd
 int sockfd;
@@ -62,9 +65,6 @@ void open_socket()
  */
 void prepare_headers(struct iphdr *ip, struct icmphdr *icmp)
 {
-  memset(ip, 0, sizeof(ip);
-  memset(icmp, 0, sizeof(icmp);
-
   ip->version = 4;
   ip->ihl = 5;
   ip->tos = 0;
@@ -77,7 +77,7 @@ void prepare_headers(struct iphdr *ip, struct icmphdr *icmp)
   icmp->code = 0;
   icmp->un.echo.sequence = rand();
   icmp->un.echo.id = rand();
-  icmp->checksum = 0;
+  icmp->checksum = 0;   
 }
 
 /**
@@ -91,25 +91,53 @@ void send_packet(struct icmp_packet *packet_details)
 
   struct iphdr *ip;
   struct icmphdr *icmp;
+  char *icmp_payload;
 
   int packet_size;
   char *packet;
+
+  struct sockaddr_in servaddr;
 
   src_addr = inet_addr(packet_details->src_addr);
   dest_addr = inet_addr(packet_details->dest_addr);
 
   packet_size = sizeof(struct iphdr) + sizeof(struct icmphdr) + packet_details->payload_size;
   packet = (char *)malloc(packet_size);
-
   if (packet == NULL) {
     printf("No memory available\n");
     close_socket();
     exit(-1);
   }
+  memset(packet, 0, packet_size);
+
+  ip = (struct iphdr *)packet;
+  icmp = (struct icmphdr *)(packet + sizeof(struct iphdr));
+  icmp_payload = (char *)(packet + sizeof(struct iphdr) + sizeof(struct icmphdr));
 
   prepare_headers(ip, icmp);
 
   ip->tot_len = htons(packet_size);
+  ip->saddr = src_addr;
+  ip->daddr = dest_addr;
 
-  icmp
+  memset(&servaddr, 0, sizeof(struct sockaddr_in));
+  servaddr.sin_family = AF_INET;
+  servaddr.sin_addr.s_addr = dest_addr;
+  
+  memcpy(icmp_payload, packet_details->payload, packet_details->payload_size);
+
+  icmp->checksum = 0;
+  icmp->checksum = in_cksum((unsigned short *)icmp, sizeof(struct icmphdr) + packet_details->payload_size);
+
+  sendto(sockfd, packet, packet_size, 0, (struct sockaddr *)&servaddr, sizeof(struct sockaddr_in));
+
+  free(packet);
+}
+
+/**
+ * Function to close socket
+ */
+void close_socket()
+{
+  close(sockfd);
 }
