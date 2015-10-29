1b9aa55f7f984268298020a6f81ced274b948084
Dhaval Kapil <me@dhavalkapil.com>
2015-10-29 16:54:25 +0530

    Updated client script and server scripts

--- client.sh
+++ client.sh
@@ -1,1 +1,9 @@
+#!/bin/sh
+
+# Assigining an IP address and mask to 'tun0' interface
 ifconfig tun0 mtu 1500 up 10.0.1.2 netmask 255.255.255.0
+
+# Modifying IP routing tables
+route del default
+route add -host <server> gw <gateway> dev <device>
+route add default gw 10.0.1.1 tun0
---
+++ server.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+# Assigining an IP address and mask to 'tun0' interface
+ifconfig tun0 mtu 1500 up 10.0.1.1 netmask 255.255.255.0 
+
+# Preventing the kernel to reply to any ICMP pings
+echo 1 | dd of=/proc/sys/net/ipv4/icmp_echo_ignore_all
+
+# Enabling IP forwarding
+echo 1 | dd of=/proc/sys/net/ipv4/ip_forward
+
+# Adding an iptables rule to masquerade for 10.0.0.0/8
+iptables -t nat -A POSTROUTING -s 10.0.0.0/8 -j MASQUERADE
