15adb42715d37274bccc7338a010226c5c1d19e3
Dhaval Kapil <me@dhavalkapil.com>
2015-11-01 04:36:33 +0530

    Added instructions in client.sh regarding server, gateway and interface

--- client.sh
+++ client.sh
@@ -5,5 +5,7 @@ ifconfig tun0 mtu 1500 up 10.0.1.2 netmask 255.255.255.0
 
 # Modifying IP routing tables
 route del default
-route add -host <server> gw <gateway> dev <device>
+# 'server' is the IP address of the proxy server
+# 'gateway' and 'interface' can be obtained by usint the command: 'route -n'
+route add -host <server> gw <gateway> dev <interface>
 route add default gw 10.0.1.1 tun0
