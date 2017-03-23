5b7bdd5628c02e526cf71f2afdf63037f37782d9
YaoYu-Li <forever3580ya@gmail.com>
2017-03-23 22:25:48 +0800

    Change the MTU size of tunnel (#23)
    * Change the receive buffer in recv_icmp_packet()
    
        Since the maximum MTU of ethernet is 1500 bytes, the MTU of tunnel
        should be not more than 1500 - iphdr_size - icmphdr_size. So the tunnel
        MTU in both client and server site are up to 1472 bytes.
    
    * fixed tun MTU in script

--- client.sh
+++ client.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 # Assigining an IP address and mask to 'tun0' interface
-ifconfig tun0 mtu 1500 up 10.0.1.2 netmask 255.255.255.0
+ifconfig tun0 mtu 1472 up 10.0.1.2 netmask 255.255.255.0
 
 # Modifying IP routing tables
 route del default
